de413e90813921549c5c37a4403eb74bc799d4cf
MarcosEstPam <marcosepamias@gmail.com>
2024-08-30 13:01:53 +0000

    avances

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,22 @@
 import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
+import {useState} from "react"
 import "../../styles/home.css";
+import reactDom from "react-dom";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
+export const Home = () =>{
+	const [name, setName] = useState({})
+	function verTodo() {
+		fetch("https://www.swapi.tech/api/people/")
+		.then((response) => response.json())
+		.then((data) =>
+			setName(data)) 
+	}
+
+ return (
+	<div className="text-center mt-5 min-vh-100">
+		<h1>Bienvenido</h1>
+		<button onClick={()=> verTodo()}>Ver Todo</button>
+		<p>Los personajes son: </p>
+		{name}
 	</div>
-);
+)};
