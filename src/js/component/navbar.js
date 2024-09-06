50293f25d91b61874d3c0503ede9087d4c09ab90
MarcosEstPam <marcosepamias@gmail.com>
2024-09-06 16:56:19 +0000

    avances

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -12,7 +12,7 @@ export const Navbar = () => {
 	}
 	return (
 		<nav className="navbar navbar-light" style={{backgroundColor: "#f5f7fa"}}>
-			<Link to="/">
+			<Link to="/home">
 				<span className="navbar-brand mb-0 mx-4 h1"><img className="rounded-circle logo" src="https://i.pinimg.com/474x/a2/a2/27/a2a227afa5d96e329085b989357b1129.jpg"/></span>
 			</Link>
 			<div className="text-dark">
