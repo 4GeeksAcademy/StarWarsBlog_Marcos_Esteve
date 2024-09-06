975299b37ed9c734eeef961525a64e73ce4827e7
MarcosEstPam <marcosepamias@gmail.com>
2024-09-06 18:27:58 +0000

    avances

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -20,7 +20,7 @@ export const Navbar = () => {
 			</div>
 			<div className="ml-auto">
 				<div className="dropdown">
-					<button className="btn btn-primary dropdown-toggle mx-4" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+					<button className="btn btn-success dropdown-toggle mx-4" type="button" data-bs-toggle="dropdown" aria-expanded="false">
 						Favorites
 					</button>
 					<ul className="dropdown-menu dropdown-menu-start">
@@ -31,7 +31,7 @@ export const Navbar = () => {
 										<a className="dropdown-item" href="#">
 											{elemento}
 										</a>
-										<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash2-fill mx-3" viewBox="0 0 16 16" onClick={(event)=> trash_botton(event,elemento)}>
+										<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-trash2-fill mx-3" viewBox="0 0 16 16" onClick={(event)=> trash_botton(event,elemento)}>
 										<path d="M2.037 3.225A.7.7 0 0 1 2 3c0-1.105 2.686-2 6-2s6 .895 6 2a.7.7 0 0 1-.037.225l-1.684 10.104A2 2 0 0 1 10.305 15H5.694a2 2 0 0 1-1.973-1.671zm9.89-.69C10.966 2.214 9.578 2 8 2c-1.58 0-2.968.215-3.926.534-.477.16-.795.327-.975.466.18.14.498.307.975.466C5.032 3.786 6.42 4 8 4s2.967-.215 3.926-.534c.477-.16.795-.327.975-.466-.18-.14-.498-.307-.975-.466z"/>
 										</svg>
 
